ce5fae28d29a87b4fb866827fd1af02b252e3b8d
fbarrientos <fbarrientos@uabc.edu.mx>
2022-03-01 16:31:37 -0800

    Add project files.

---
+++ Conversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+            int num1 = 5, num2, resultado;
+
+            //Preguntamos al usuario el número
+            Console.Write("Dame un número para sumarlo: ");
+            //Se asigna la variable de tipo string
+            cadena = Console.ReadLine();
+            num2 = Int32.Parse(cadena);
+            //num2 = Convert.ToInt32(cadena);
+            //Realizar la suma
+            resultado = num1 + num2;
+            Console.WriteLine("El resultado es: " + resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ReadLine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            //Preguntamos el nombre del usuario
+            Console.WriteLine("Cómo te llamas: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola" + nombre + "Quieres ver el menu?");
+            Console.ReadKey();
+
+
+        }
+    }
+}
